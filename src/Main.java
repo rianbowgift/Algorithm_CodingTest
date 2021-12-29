7248e873f907d6a367bf6ccce92dc07bebe4f6c2
YakultLover <rianbowgift@gmail.com>
2021-12-29 13:03:54 +0900

    good.

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,15 @@ import Programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = new int[]{1,2,3,4,5};
+        int[][] a = new int[][]{{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] b = new int[]{1,5,3,5,1,2,1,4};
 
 
-        int c[] = new PM_12906().solution(a);
-        //System.out.println(c);
+        int c = new PM_64061().solution(a,b);
+        System.out.println(c);
 
-        for(int i=0;i<c.length;i++){
-            System.out.println(c[i]);
-        }
+//        for(int i=0;i<c.length;i++){
+//            System.out.println(c[i]);
+//        }
     }
 }
