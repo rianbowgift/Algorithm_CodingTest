c5806a649674d7f29be9ee7d873998b3d8deef57
YakultLover <rianbowgift@gmail.com>
2021-12-29 10:39:48 +0900

    good.

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,14 @@ import Programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = new int[]{1,2,7,6,4};
+        int[] a = new int[]{1,2,3,4,5};
 
 
-        int c = new PM_12977().solution(a);
-        System.out.println(c);
+        int c[] = new PM_12906().solution(a);
+        //System.out.println(c);
+
+        for(int i=0;i<c.length;i++){
+            System.out.println(c[i]);
+        }
     }
 }
