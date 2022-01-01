bfe7ac30dede5698b0c3b91d9fbc915752104276
YakultLover <rianbowgift@gmail.com>
2022-01-01 14:12:17 +0900

    good.

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,23 @@ import Programmers.*;
 
 public class Main {
     public static void main(String[] args) {
-        String a = "a B z";
-        int b = 4;
+        String[] a = new String[]{"abzcd","cdzab","abzfg","abzaa","abzbb","bbzaa"};
+        //String[] a = new String[]{"abzcd","abzfg","abzaa","abzbb"};
+        int b = 2;
 
 
 
-        String z = new PM_12926().solution(a,b);
+        String[] z = new PM_12915().solution(a,b);
 
 
 
-        System.out.println(z);
+//        System.out.println(z);
 
 
 
-//        for(int i=0;i<z.length;i++){
-//            System.out.println(z[i]);
-//        }
+        for(int i=0;i<z.length;i++){
+            System.out.println(z[i]);
+        }
 
 
 //
