dbf3c4bf96ea758b092be36cec3c883242b8578c
YakultLover <rianbowgift@gmail.com>
2022-01-01 12:42:06 +0900

    good.

---
+++ src/Programmers/PM_12922.java
@@ -0,0 +1,21 @@
+package Programmers;
+//https://programmers.co.kr/learn/courses/30/lessons/12922
+public class PM_12922 {
+    public String solution(int n) {
+        String answer = "";
+
+        for(int i=1; i<=n; i++){
+
+                if(i%2==0){ //짝수면 박
+                answer+= "박";
+                }else{
+                    answer+= "수";
+                }
+
+        }
+
+
+        return answer;
+    }
+
+}
