98af20a61c71f74920ca839e897c6d6b225fa24d
YakultLover <rianbowgift@gmail.com>
2022-01-06 10:50:51 +0900

    good.

---
+++ src/Programmers/Level1/PM_42840.java
@@ -0,0 +1,12 @@
+package Programmers.Level1;
+//https://programmers.co.kr/learn/courses/30/lessons/42840
+
+public class PM_42840 {
+    public int[] solution(int[] answers) {
+        int[] answer = {};
+        return answer;
+    }
+
+
+
+}
