b3538d22ca0fdb79d43ec54a14381d43856d1e3d
YakultLover <rianbowgift@gmail.com>
2022-02-09 12:18:22 +0900

    good.

---
+++ src/Baekjoon/BJ_1002.java
@@ -0,0 +1,20 @@
+package Baekjoon;
+//https://www.acmicpc.net/problem/1002
+public class BJ_1002 {
+    public static void main(String[] args){
+        int x1=0;
+        int y1=0;
+        int r1=13;
+
+        int x2=40;
+        int y2=0;
+        int r2=37;
+
+
+
+
+
+
+    }
+
+}
--- src/Programmers/Level2/PM_12953.java
+++ src/Programmers/Level2/PM_12953.java
@@ -1,4 +1,4 @@
-package Programmers.Level2.Failure;
+package Programmers.Level2;
 //https://programmers.co.kr/learn/courses/30/lessons/12953
 //좀더 공부하고...
 public class PM_12953 {
