dc87b1e0cb5f31bff6a493f29a63b23fa405c738
YakultLover <rianbowgift@gmail.com>
2022-02-13 05:02:21 +0900

    백준알고리즘 새벽에 안졸려서 쉬운거 가볍게..

---
+++ src/Baekjoon/Bronze/BJ_2292.java
@@ -0,0 +1,36 @@
+package Baekjoon.Bronze;
+
+import java.util.Scanner;
+
+//https://www.acmicpc.net/problem/2292
+//맞추긴 했지만 이방식이 아닌듯하다..새벽에 머리가 안굴러간듯
+
+
+public class BJ_2292 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int rooms = 1; //넘긴방수
+        int big =0;
+        int cnt = 1; //계수
+
+        if(num==1){
+            System.out.println("1");
+            return;
+        }
+
+        while ( num-1>big){
+            big+= 6*cnt;
+            cnt++;
+            rooms++;
+        }
+
+        System.out.println(rooms);
+
+
+    }
+
+}
