73ca2f0c9953a5f9782245bdd68405e2fc67ac33
YakultLover <rianbowgift@gmail.com>
2022-02-22 05:01:36 +0900

    카카오 2018코테 단계별로 풀기

---
+++ src/Programmers/PM_17678.java
@@ -0,0 +1,14 @@
+package Programmers;
+//https://programmers.co.kr/learn/courses/30/lessons/17678
+public class PM_17678 {
+
+    public String solution(int n, int t, int m, String[] timetable) {
+        String answer = "";
+
+
+
+
+        return answer;
+    }
+
+}
