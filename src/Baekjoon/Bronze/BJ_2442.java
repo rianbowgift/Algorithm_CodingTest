86053256755e25b10a50193ad2dcaa34b2e65c15
YakultLover <rianbowgift@gmail.com>
2022-02-27 02:54:07 +0900

    프로그래머스1단계 남은문제

---
+++ src/Baekjoon/Bronze/BJ_2442.java
@@ -0,0 +1,25 @@
+package Baekjoon.Bronze;
+//https://www.acmicpc.net/problem/2442
+
+import java.util.Scanner;
+
+public class BJ_2442 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            for (int j = a - i; j > 1 ; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (i * 2) + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
