773803bd3d123bea391bfd1bd88a6753b4aec9b2
YakultLover <rianbowgift@gmail.com>
2022-03-08 00:00:58 +0900

    간단히 넘김

--- src/Baekjoon/Bronze/Success/BJ_1985.java
+++ src/Baekjoon/Bronze/Success/BJ_1985.java
@@ -8,6 +8,7 @@ public class BJ_1985 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
