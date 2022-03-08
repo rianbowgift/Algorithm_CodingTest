f8434722c45699251955f8f9b9a811508202b8c5
YakultLover <rianbowgift@gmail.com>
2022-03-09 00:01:44 +0900

    메모리초과. 이유를 알수없음

--- src/Baekjoon/Silver/Failure/BJ_1312.java
+++ src/Baekjoon/Silver/Failure/BJ_1312.java
@@ -1,22 +1,36 @@
 package Baekjoon.Silver.Failure;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+//import java.util.Scanner;
+//https://www.acmicpc.net/problem/1312
+//참고 https://jsonobject.tistory.com/466
 
 public class BJ_1312 {
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        long a = sc.nextInt();
-        long b = sc.nextInt();
-        long c = sc.nextInt();
-        double x = (double) a/ (double) b;
-        long z = 0;
-        for(long i=0;i<c;i++){
-            x= x*10;
-        }
+    public static void main(String[] args) throws IOException {
 
-        z = (long) x;
-        String y = Long.toString(z);
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String s = bf.readLine();//문자열 입력 받을 때
+        String[] m = s.split(" ");
+        BigDecimal a = new BigDecimal(m[0]);
+        BigDecimal b = new BigDecimal(m[1]);
+        int c = Integer.parseInt(m[2]);
+
+
+//
+//        Scanner sc = new Scanner(System.in);
+//        double a = sc.nextDouble();
+//        double b = sc.nextDouble();
+//        int c = sc.nextInt();
+
+
+        BigDecimal x = a.divide(b,c,RoundingMode.FLOOR);
+        String y = x.toString();
+        //System.out.println(y);
 
         System.out.print(y.charAt(y.length()-1));
 
