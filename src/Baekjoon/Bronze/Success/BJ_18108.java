926ac125664e45d0624159e96e6002ac30fc2a8c
YakultLover <rianbowgift@gmail.com>
2022-03-11 00:37:23 +0900

    :(

---
+++ src/Baekjoon/Bronze/Success/BJ_18108.java
@@ -0,0 +1,15 @@
+package Baekjoon.Bronze.Success;
+
+import java.util.Scanner;
+//https://www.acmicpc.net/problem/18108
+public class BJ_18108 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num-543);
+
+
+    }
+}
